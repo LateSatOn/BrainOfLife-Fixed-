e4f89c16347dda0cdbce2398af33ea84d66d11dc
Layth Sattam <154368820+LateSatOn@users.noreply.github.com>
2024-10-01 15:55:08 -0400

    Update Logic.py

--- Logic.py
+++ Logic.py
@@ -1,6 +1,7 @@
 def cell_logic(cell_list, i, j):
     cell = cell_list[i][j]
     enabled_transistors = cell_list[i][j].find_enabled_transistors()
+    enabled_resting_neurons = cell_list[i][j].find_enabled_resting_neurons()
 
     if cell.get_state() == 1 and cell.get_enabled() == 0:
         for n in range(len(enabled_transistors)):
@@ -14,12 +15,24 @@ def cell_logic(cell_list, i, j):
             print(f"{n}")
             enabled_transistors[n].change_signal_by(1)
             enabled_transistors[n].change_enabled_by(1)
+        for n in range(len(enabled_resting_neurons)):
+            print(f"{n}")
+            enabled_resting_neurons[n].change_signal_by(1)
+            enabled_resting_neurons[n].change_enabled_by(1)
 
         cell.set_signal(0)
         cell.set_enabled(2)
 
-    elif cell.get_state() == 3 and cell.get_enabled() == 0:
-        cell.set_enabled(2)
+    elif cell.get_state() == 3 and cell.get_enabled() == 0 and cell.get_signal() > 0:
+        cell.set_state(4)
+
+        cell.set_enabled(1)
 
     elif cell.get_state() == 4 and cell.get_enabled() == 0:
+        for n in range(len(enabled_transistors)):
+            enabled_transistors[n].change_signal_by(1)
+            enabled_transistors[n].change_enabled_by(1)
+        cell.set_state(3)
+
+        cell.set_signal(0)
         cell.set_enabled(2)
